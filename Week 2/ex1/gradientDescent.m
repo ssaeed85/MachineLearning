95285df0d30a2c0833ef5c6feb8a4cbe0016001d
SSaeed <saadsaeed85@gmail.com>
2015-12-08 11:06:05 -0600

    Added Gradient Descent Function

--- Week 2/ex1/gradientDescent.m
+++ Week 2/ex1/gradientDescent.m
@@ -17,11 +17,15 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    temp1 =0;
+    temp2 =0;
+    for i=1:m
+        temp1 = temp1 + ( theta(1)*X(i,1) + theta(2) * X(i,2) - y(i))*X(i,1);
+        temp2 = temp2 + ( theta(1)*X(i,1) + theta(2) * X(i,2) - y(i))*X(i,2);
+    end
+    
+    theta(1) = theta(1) - alpha * temp1 / m;
+    theta(2) = theta(2) - alpha * temp2 / m;
 
     % ============================================================
 
