de2978ab91e075fac15f3f1e07a6872a765983d8
SSaeed <saadsaeed85@gmail.com>
2015-12-08 22:58:13 -0600

    Added normalization for multivariate function

--- Week 2/ex1/featureNormalize.m
+++ Week 2/ex1/featureNormalize.m
@@ -26,11 +26,12 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
-
-
+sigma = std(X);
+mu = mean(X);
+for i=1:size(X,2)
+    X_norm(:,i)=X(:,i)-mu(i)
+    X_norm(:,i)=X_norm(:,i)/sigma(i)
+end
 
 
 
