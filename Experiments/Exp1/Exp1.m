a2e8e926dca2dc46cca70c62a4c66504c80efcdd
SSaeed <saadsaeed85@gmail.com>
2016-01-18 12:02:26 -0600

    Added few files to help with exp

---
+++ Experiments/Exp1/Exp1.m
@@ -0,0 +1,8 @@
+%% Machine Learning Online Class
+%  Experiment 1 | Effect of threshold value change
+%
+%  This experiment will evaluate the effect of the change in threshold 
+%  value for the sigmoid function on the various machine learning algorithm
+%  evaluation metrics, namely: Precision, Recall, Accuracy and F-score
+
+%% Initialization
---
+++ Experiments/Exp1/sigmoid.m
@@ -0,0 +1,23 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+[m,n]=size(z);
+
+for j=1:n
+    for i=1:m
+        g(i,j)=1/(1+exp(-z(i,j)));
+    end
+end
+
+
+% =============================================================
+
+end
